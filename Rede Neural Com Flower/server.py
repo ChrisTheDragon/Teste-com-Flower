f2507ff804e3e9359d6b257b4a5da734408a56bd
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-11-16 10:43:26 -0300

    Add weighted average evaluation metric aggregation function to FedAvg strategy.

--- Rede Neural Com Flower/server.py
+++ Rede Neural Com Flower/server.py
@@ -1,7 +1,14 @@
 import flwr as fl
 
+def weighted_average(metrics):
+    acuracias = [num_exemplos * m["acuracia"] for num_exemplos, m in metrics]
+    exemplos = [num_exemplos for num_exemplos, _ in metrics]
+    return {"acuracia": sum(acuracias) / sum(exemplos)}
+
 fl.server.start_server(
     server_address="[::]:8080",
     config=fl.server.ServerConfig(num_rounds=3),
-    strategy=fl.server.strategy.FedAvg(),
+    strategy=fl.server.strategy.FedAvg(
+        evaluate_metrics_aggregation_fn=weighted_average
+        ),
 )
