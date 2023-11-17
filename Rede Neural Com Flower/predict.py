f4aa8a6f50f75c0acba680f823f61b2822ccaa60
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-11-17 15:46:24 -0300

    Add predict_prob function for probability prediction

--- Rede Neural Com Flower/predict.py
+++ Rede Neural Com Flower/predict.py
@@ -45,6 +45,25 @@ def predict_image(model, image_path):
     
     return predicted_class.item()
 
+def predict_prob(model, image_path):
+    model.eval()  # Modo de avaliação
+    
+    # Processar a imagem
+    input_image = process_image(image_path)
+    
+    # Enviar a imagem para o dispositivo (CPU ou GPU)
+    input_image = input_image.to(DEVICE)
+    
+    output = model(input_image)
+    
+    probs = torch.nn.functional.softmax(output, dim=1) * 100
+    prob_dict = {}
+    for i, classname in enumerate(CLASSES):
+        prob = probs[0][i].item()
+        print(f"{classname} probabilidade: {prob:.2f}")
+        prob_dict[classname] = [prob]
+    
+
 # Função para carregar o modelo a partir de um arquivo
 def load_model_from_file(file_path):
     modelo = load_model()  # Use a função adequada para criar o modelo inicial
@@ -60,6 +79,16 @@ if __name__ == '__main__':
     trained_model = load_model_from_file("modelo_treinado.pth")
     
     # Fazer a predição
-    predicted_class = predict_image(trained_model, image_path)
+    op = input('Digite 1 para predição de classe ou 2 para predição de probabilidade: ')
+    if op == '1':
+        predicted_class = predict_image(trained_model, image_path)
+        print(f"A imagem é da classe: {CLASSES[predicted_class]}")
+    elif op == '2':
+        predict_prob(trained_model, image_path)
+    else:
+        print('Opção inválida!')
     
-    print(f"A imagem é da classe: {CLASSES[predicted_class]}")
+    #predicted_class = predict_image(trained_model, image_path)
+    #predicted_class = predict_prob(trained_model, image_path)
+    
+    #print(f"A imagem é da classe: {CLASSES[predicted_class]}")
